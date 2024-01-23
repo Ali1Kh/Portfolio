4b46cf986e28a88c46ca34dd730c8583c305df9e
Ali Khaled <alielsaadany94@gmail.com>
2024-01-23 21:03:28 +0200

    -

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ dotenv.config();
 const app = express();
 const port = process.env.port;
 app.use(express.json());
-
 await dbConnect();
-
 app.use("/projects", projectsRouter);
-
 app.use((error,req,res,next)=>{
     return res.json({success:false,error})
 })
-
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
