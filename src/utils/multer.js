b00bb46889d272d02a1217be95567c04550e7a2d
Ali Khaled <alielsaadany94@gmail.com>
2024-01-23 20:38:44 +0200

    -

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -2,6 +2,5 @@ import multer, { diskStorage } from "multer";
 export function uploadFiles() {
     const storage = diskStorage({});
     const multerUpload = multer({ storage });
-
     return multerUpload;
 }
