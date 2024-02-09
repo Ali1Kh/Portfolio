924208e7f7fc18e37f23a916ed082ade4f9b01ce
Ali Khaled <alielsaadany94@gmail.com>
2024-02-09 22:48:35 +0200

    wakatime

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import projectsRouter from "./src/modules/projects/projects.router.js";
 import certificatesRouter from "./src/modules/certificates/certificates.router.js";
 import mailsRouter from "./src/modules/mails/mails.router.js";
 import cors from "cors";
+import axios from 'axios'
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -13,12 +14,25 @@ app.use(express.json());
 await dbConnect();
 app.use("/projects", projectsRouter);
 app.use("/certificates", certificatesRouter);
-app.use("/mails", mailsRouter)
+app.use("/mails", mailsRouter);
 app.all("/uptime", (req, res) => {
-    console.log("Up Time Requested");
-    res.status(200).send("success");
-  });
+  console.log("Up Time Requested");
+  res.status(200).send("success");
+});
+app.get("/wakatime/getHours", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.wakatime.com/api/v1/users/ali1kh/all_time_since_today?api_key=waka_0c7cd19e-ce2a-4093-a381-5cc46c195098",
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json(error.response.data);
+  }
+});
 app.use((error, req, res, next) => {
-    return res.json({ success: false, error: { message: error.message, stack: error.stack } })
-})
+  return res.json({
+    success: false,
+    error: { message: error.message, stack: error.stack },
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
