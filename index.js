f7a7406f57efad0089989648b54dd6655455f1fa
Ali Khaled <alielsaadany94@gmail.com>
2024-02-09 22:49:35 +0200

    -

--- index.js
+++ index.js
@@ -19,16 +19,7 @@ app.all("/uptime", (req, res) => {
   console.log("Up Time Requested");
   res.status(200).send("success");
 });
-app.get("/wakatime/getHours", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://api.wakatime.com/api/v1/users/ali1kh/all_time_since_today?api_key=waka_0c7cd19e-ce2a-4093-a381-5cc46c195098",
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(error.response.status).json(error.response.data);
-  }
-});
+
 app.use((error, req, res, next) => {
   return res.json({
     success: false,
