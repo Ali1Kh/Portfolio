fa51cfc67fb85979b745af04eec22bf121bdec83
Ali Khaled <alielsaadany94@gmail.com>
2025-03-19 06:33:41 +0200

    animate follow me

---
+++ src/components/FollowMe/FollowMe.jsx
@@ -0,0 +1,60 @@
+import "./followMe.css";
+import React from "react";
+import $ from "jquery";
+export default function FollowMe() {
+  let itemHeight = 140;
+  function dragEnd() {
+    document.documentElement.style.setProperty("--socialTop", `18px`);
+    $(".socialmedia").addClass("animate__flipInX");
+  }
+  return (
+    <div className="socialmedia my-2 animate__animated animate__flipInX animate__fast">
+      <p
+        draggable
+        onDragStart={(e) => e.dataTransfer.setDragImage(new Image(), 0, 0)}
+        onDrag={(e) => {
+          if (
+            e.clientY < window.innerHeight / 3 &&
+            e.clientY - itemHeight > 18
+          ) {
+            document.documentElement.style.setProperty(
+              "--socialTop",
+              `${e.clientY - itemHeight}px`
+            );
+          } else if (e.clientY == 0) {
+            dragEnd();
+          }
+          $(".socialmedia").removeClass("animate__flipInX");
+        }}
+        onDragEnd={dragEnd}
+        className="followMe sunFont m-0 "
+      >
+        Follow Me
+      </p>
+      <div className="socialItems pb-3 d-flex flex-column gap-2">
+        <a
+          className="text-white"
+          href="https://www.linkedin.com/in/ali1kh/"
+          target="_blank"
+        >
+          <i className="fa fs-5 fa-brands fa-linkedin-in"></i>
+        </a>
+        <a
+          className="text-white"
+          href="https://www.github.com/ali1kh"
+          target="_blank"
+          s
+        >
+          <i className="fa fs-5 fa-brands fa-github"></i>
+        </a>
+        <a
+          href="https://twitter.com/aly1kh"
+          target="_blank"
+          className="text-white"
+        >
+          <i className="fa-brands fs-5 fa-x-twitter"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,13 +2,11 @@ import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import "./layout.css";
-import Alert from "@mui/material/Alert";
-import Stack from "@mui/material/Stack";
-import $ from "jquery";
+import FollowMe from "../FollowMe/FollowMe";
 export default function Layout() {
   return (
     <>
-{/*       <Stack
+      {/*       <Stack
         className="overflow-hidden alertContainer  "
         sx={{
           width: "100%",
@@ -30,37 +28,9 @@ export default function Layout() {
         </Alert>
       </Stack> */}
       <Navbar />
-      
+
       <Outlet />
-      <div className="socialmedia my-2 animate__animated animate__flipInX animate__fast">
-        <p className="followMe sunFont m-0 animate__animated /animate__rollIn ">
-          Follow Me
-        </p>
-        <div className="socialItems pb-3 d-flex flex-column gap-2">
-          <a
-            className="text-white"
-            href="https://www.linkedin.com/in/ali1kh/"
-            target="_blank"
-          >
-            <i className="fa fs-5 fa-brands fa-linkedin-in"></i>
-          </a>
-          <a
-            className="text-white"
-            href="https://www.github.com/ali1kh"
-            target="_blank"
-            s
-          >
-            <i className="fa fs-5 fa-brands fa-github"></i>
-          </a>
-          <a
-            href="https://twitter.com/aly1kh"
-            target="_blank"
-            className="text-white"
-          >
-            <i className="fa-brands fs-5 fa-x-twitter"></i>
-          </a>
-        </div>
-      </div>
+      <FollowMe />
     </>
   );
 }
