9e57266be216b23a6cf60e4d2841767999b58347
Ali Elsaadany <alielsaadany94@gmail.com>
2025-05-16 16:26:51 +0300

    Update LatestProjects.jsx

--- src/components/LatestProjects/LatestProjects.jsx
+++ src/components/LatestProjects/LatestProjects.jsx
@@ -16,7 +16,7 @@ export default function LatestProjects() {
     <section className="projects py-2 my-5 mt-3">
       <div className="container ">
         <div className="sectionTitle text-center primaryText mb-4">
-          <h2>Latest Projects</h2>
+          <h2>Highlighted Projects</h2>
         </div>
         <div className="projectsCards">
           <Projects projects={latestProjects} />
