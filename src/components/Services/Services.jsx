f7fc73c29c9b82bf834dcd571b572fa6feba1de1
Ali Khaled <alielsaadany94@gmail.com>
2025-07-08 16:19:29 +0300

    new services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,70 +1,86 @@
 import React from "react";
 
 export default function Services() {
+  let services = [
+    {
+      icon: "laptop-code",
+      tile: "Web Development",
+      description:
+        "Full featured websites that turn ideas into real working solutions.",
+    },
+    {
+      imageIcon: require("../../imgs/icons/rwd.png"),
+      tile: "Responsive Web Design",
+      description: "Fully responsive websites designed to work on any device.",
+    },
+    {
+      icon: "database",
+      tile: "Database Design and Management",
+      description: "Easy to manage systems that keep your business data safe.",
+    },
+    {
+      icon: "palette",
+      tile: "UI/UX Design",
+      description:
+        "Simple and clean interfaces crafted for smooth user experiences.",
+    },
+    {
+      icon: "microchip",
+      tile: "Embedded Systems",
+      description:
+        "Smart embedded hardware solutions for real world systems.",
+    },
+    {
+      icon: "desktop",
+      tile: "Desktop Application Development",
+      description:
+        "Custom desktop applications that run smoothly and offline.",
+    },
+  ];
   return (
     <section className="services pb-5 py-2 my-5 mt-3">
       <div className="container ">
         <div className="sectionTitle text-center primaryText mb-5 my-2 pt-3">
           <h2>Services</h2>
         </div>
-        <div className="row gy-3">
-          <div className="col-md-4">
-            <div className="serviceItem h-100 d-flex align-items-center flex-column borderGrey shadow p-5 px-4 rounded-3">
-              <div
-                className="serviceIcon mb-3 border rounded-circle text-white d-flex justify-content-center align-items-center"
-                style={{
-                  width: "55px",
-                  height: "55px",
-                }}
-              >
-                <i className="fa fa-laptop-code fs-4"></i>
+        <div className="row gy-3 justify-content-center">
+          {services.map((serviceItem) => (
+            <>
+              <div className="col-md-6 col-xl-4">
+                <div className="serviceItem text-center h-100 d-flex align-items-center flex-column borderGrey shadow p-5 pb-4 px-4 rounded-3">
+                  <div
+                    className="serviceIcon mb-3 border rounded-circle text-white d-flex justify-content-center align-items-center"
+                    style={{
+                      width: "55px",
+                      height: "55px",
+                    }}
+                  >
+                    {serviceItem.icon ? (
+                      <i className={`fa fa-${serviceItem.icon} fs-4`}></i>
+                    ) : (
+                      <img
+                        src={serviceItem.imageIcon}
+                        width={30}
+                        height={30}
+                        alt=""
+                      />
+                    )}
+                  </div>
+                  <h5
+                    style={{
+                      whiteSpace: "nowrap",
+                    }}
+                    className="serviceTitle"
+                  >
+                    {serviceItem.tile}
+                  </h5>
+                  <p className="serviceInfo fst-normal fw-light text-center px-2">
+                    {serviceItem.description}
+                  </p>
+                </div>
               </div>
-              <h5 className="serviceTitle">Web Development</h5>
-              <p className=" serviceInfo fst-normal fw-light text-center px-2">
-                From front-end design to back-end architecture,
-                {/* <br />*/}I build it all. Custom Web Applications Made to
-                Perform.
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="serviceItem h-100 d-flex align-items-center flex-column borderGrey shadow p-5 px-3 rounded-3">
-              <div
-                className="serviceIcon mb-3 border rounded-circle text-white d-flex justify-content-center align-items-center"
-                style={{
-                  width: "55px",
-                  height: "55px",
-                }}
-              >
-                <i className="fa fa-display fs-4"></i>
-              </div>
-              <h5 className="serviceTitle">Responsive Web Design</h5>
-              <p className="serviceInfo fst-normal fw-light text-center px-1">
-                Build websites that seamlessly adjust to any screen size,
-                ensuring a flawless user experience across desktops, tablets,
-                and mobiles.
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="serviceItem h-100 d-flex align-items-center flex-column borderGrey shadow p-5 px-3 rounded-3">
-              <div
-                className="serviceIcon mb-3 border rounded-circle text-white d-flex justify-content-center align-items-center"
-                style={{
-                  width: "55px",
-                  height: "55px",
-                }}
-              >
-                <i className="fa fa-database fs-4"></i>
-              </div>
-              <h5 className="serviceTitle">Database Design and Management</h5>
-              <p className="serviceInfo fst-normal fw-light text-center px-1">
-                Design and manage databases that organize your information
-                beautifully, making it easy to access, analyze, and unlock
-                hidden insights.
-              </p>
-            </div>
-          </div>
+            </>
+          ))}
         </div>
       </div>
     </section>
