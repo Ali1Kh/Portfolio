5105635f8be79bd02702d53ab1f41a96b0ac044a
Ali Khaled <alielsaadany94@gmail.com>
2025-09-01 21:30:20 +0300

    companies

--- src/components/certificatedIn/CertificatedIn.jsx
+++ src/components/certificatedIn/CertificatedIn.jsx
@@ -7,7 +7,7 @@ export default function CertificatedIn() {
   const companiesSlider = {
     dots: false,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
     speed: 1000,
@@ -16,6 +16,65 @@ export default function CertificatedIn() {
   };
   const navigate = useNavigate();
 
+  let companies = [
+    {
+      name: "Microsoft",
+      img: require("../../imgs/certificatedFrom/microsoft.png"),
+    },
+    {
+      name: "Oracle Academy",
+      img: require("../../imgs/certificatedFrom/oracle.png"),
+    },
+
+    {
+      name: "Route Academy",
+      img: require("../../imgs/certificatedFrom/route.jpg"),
+    },
+    {
+      name: "Mica Egypt",
+      img: require("../../imgs/certificatedFrom/mica.jpg"),
+    },
+    {
+      name: "Information Technology Institute (ITI)",
+      img: require("../../imgs/companies/iti.png"),
+    },
+    {
+      name: "National telecommunicarion institute",
+      img: require("../../imgs/companies/nti.png"),
+    },
+    {
+      name: "Information Technology Industry Development Agency (ITIDA)",
+      img: require("../../imgs/certificatedFrom/itida.png"),
+    },
+    {
+      name: "Sprints",
+      img: require("../../imgs/certificatedFrom/sprints.png"),
+    },
+    {
+      name: "Innovegypt",
+      img: require("../../imgs/certificatedFrom/innovegypt.png"),
+    },
+    {
+      name: "Technology Innovation & Entrepreneurship Center (TIEC)",
+      img: require("../../imgs/certificatedFrom/tiec.png"),
+    },
+    {
+      name: "NVIDIA",
+      img: require("../../imgs/certificatedFrom/nvidia.png"),
+    },
+    {
+      name: "HackerRank",
+      img: require("../../imgs/certificatedFrom/hackerrank.png"),
+    },
+    {
+      name: "freeCodeCamp",
+      img: require("../../imgs/certificatedFrom/freecodecamp.jpeg"),
+    },
+    {
+      name: "SoloLearn",
+      img: require("../../imgs/certificatedFrom/sololearn.jpg"),
+    },
+  ];
   return (
     <section className="certificates secBg my-5 mt-3">
       <div className="container py-5">
@@ -25,72 +84,20 @@ export default function CertificatedIn() {
         <div className="companies gap-4 mb-3 d-flex flex-column justify-content-center align-items-center">
           <div
             className="slidesContainer text-center"
-            style={{ width: "350px" }}
+            style={{ width: "520px" }}
           >
             <Slider {...companiesSlider}>
-              <div className="companyItem d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/microsoft.png")}
-                  alt=""
-                  title="Microsoft"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
-              <div className="companyItem  d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/oracle.png")}
-                  alt=""
-                  title="Oracle Academy"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
-              <div className="companyItem d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/freecodecamp.jpeg")}
-                  alt=""
-                  title="freeCodeCamp"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
-              <div className="companyItem d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/hackerrank.png")}
-                  alt=""
-                  title="HackerRank"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
-              <div className="companyItem d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/route.jpg")}
-                  alt=""
-                  title="Route Academy"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
-              <div className="companyItem d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/sololearn.jpg")}
-                  alt=""
-                  title="SoloLearn"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
-              <div className="companyItem d-flex justify-content-center align-items-center">
-                <img
-                  className="w-100 rounded-circle cursorGrab"
-                  src={require("../../imgs/certificatedFrom/mica.jpg")}
-                  alt=""
-                  title="Mica Egypt"
-                  style={{ maxWidth: "80px" }}
-                />
-              </div>
+              {companies.map((company, index) => (
+                <div className="companyItem d-flex justify-content-center align-items-center">
+                  <img
+                    className="w-100 rounded-circle cursorGrab"
+                    src={company.img}
+                    alt={company.name}
+                    title={company.name}
+                    style={{ maxWidth: "80px" }}
+                  />
+                </div>
+              ))}
             </Slider>
           </div>
           <div className="sectionFooter mt-4 mb-0 text-center">
