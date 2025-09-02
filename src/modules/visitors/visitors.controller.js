a87029f25d903624a88cbeeb6d611ea037488da9
Ali Khaled <alielsaadany94@gmail.com>
2025-09-02 23:44:22 +0300

    filter vercel visits

--- src/modules/visitors/visitors.controller.js
+++ src/modules/visitors/visitors.controller.js
@@ -1,6 +1,8 @@
 import { Visitors } from "../../../DB/models/visitors.model.js";
 
 export const getVisitors = async (req, res, next) => {
-  let visitors = await Visitors.find().sort({ createdAt: -1 });
+  let visitors = await Visitors.find({
+    device: { $not: /vercel/i },
+  }).sort({ createdAt: -1 });
   return res.json({ success: true, results: visitors, count: visitors.length });
 };
