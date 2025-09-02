1bf4ad8ab360c494b04c26663bddca59a57fd450
Ali Khaled <alielsaadany94@gmail.com>
2025-09-03 00:07:50 +0300

    sort visitors

--- src/modules/visitors/visitors.controller.js
+++ src/modules/visitors/visitors.controller.js
@@ -3,6 +3,6 @@ import { Visitors } from "../../../DB/models/visitors.model.js";
 export const getVisitors = async (req, res, next) => {
   let visitors = await Visitors.find({
     device: { $not: /vercel/i },
-  }).sort({ createdAt: -1 });
+  }).sort({ updatedAt: -1 });
   return res.json({ success: true, results: visitors, count: visitors.length });
 };
