86b83fe291e1aef56ef6af953fd61731c4fd25b0
Ali Elsaadany <alielsaadany94@gmail.com>
2025-09-11 01:43:16 +0300

    Update visitors.controller.js

--- src/modules/visitors/visitors.controller.js
+++ src/modules/visitors/visitors.controller.js
@@ -2,7 +2,7 @@ import { Visitors } from "../../../DB/models/visitors.model.js";
 
 export const getVisitors = async (req, res, next) => {
   let visitors = await Visitors.find({
-    device: { $not: /vercel/i },
+    device: { $not: /(vercel|meta-externalagent|Googlebot)/i },
   }).sort({ updatedAt: -1 });
   return res.json({ success: true, results: visitors, count: visitors.length });
 };
